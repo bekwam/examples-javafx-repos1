b2e2b6953cb51d1aa166e0ec7eb0ad24ae425d70
walkerca <carl@bekwam.com>
2015-06-09 22:24:05 -0400

    Added source file header comments

--- examples-javafx-parent/examples-javafx-oldscores/src/main/java/com/bekwam/examples/javafx/oldscores/HelpDialog.java
+++ examples-javafx-parent/examples-javafx-oldscores/src/main/java/com/bekwam/examples/javafx/oldscores/HelpDialog.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 Bekwam, Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.bekwam.examples.javafx.oldscores;
 
 import javafx.fxml.FXMLLoader;
@@ -10,7 +25,9 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 /**
- * Created by carl_000 on 5/31/2015.
+ * Dialog for Help screen
+ *
+ * @author carl_000
  */
 public class HelpDialog {
 
